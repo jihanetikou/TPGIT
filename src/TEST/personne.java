a27468abf0b18dd513ee24760c50216051a3e0b5
cherif computer <cherif computer@DESKTOP-8HNVMMU>
2021-03-01 11:59:12 +0100

    enlever un get

--- src/TEST/personne.java
+++ src/TEST/personne.java
@@ -8,9 +8,7 @@ public personne(String nom, String prenom) {
 	this.nom = nom;
 	this.prenom = prenom;
 }
-public String getNom() {
-	return nom;
-}
+
 public void setNom(String nom) {
 	this.nom = nom;
 }
