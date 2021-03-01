cca8a55618f06324b3ee5b1f6b1ed2f859cfa27d
cherif computer <cherif computer@DESKTOP-8HNVMMU>
2021-03-01 11:49:49 +0100

    initialiser le projet

---
+++ src/TEST/personne.java
@@ -0,0 +1,28 @@
+package TEST;
+
+public class personne {
+String nom;
+String prenom;
+public personne(String nom, String prenom) {
+	super();
+	this.nom = nom;
+	this.prenom = prenom;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+@Override
+public String toString() {
+	return "personne [nom=" + nom + ", prenom=" + prenom + "]";
+}
+
+}
